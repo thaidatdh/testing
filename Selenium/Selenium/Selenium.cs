067ab2c27b5d532b95e239578ae2decf3c299713
Dat Ho <thaidatdh@gmail.com>
2020-10-27 06:42:46 +0700

    try selenium on basic calculator

---
+++ Selenium/Selenium/Selenium.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Selenium
+{
+   public class Selenium
+   {
+      IWebDriver driver;
+      bool isChrome = false;
+
+      public Selenium(bool isChrome = true) { this.isChrome = isChrome; }
+      [SetUp]
+      public void startBrowser()
+      {
+         driver = new ChromeDriver(@"P:\Testing\Selenium\");
+      }
+      public void Maximize()
+      {
+         if (driver != null)
+            driver.Manage().Window.Maximize();
+      }
+
+      [Test]
+      public void test()
+      {
+         if (driver != null)
+            driver.Url = "http://www.google.com";
+      }
+
+      public void OpenUrl(string url)
+      {
+         if (driver != null)
+            driver.Url = url;
+      }
+      public void SendKeysByXPath(string xPath, string key)
+      {
+         IWebElement element = driver.FindElement(By.XPath(xPath));
+         element.SendKeys(key);
+      }
+      public void SummitByXPath(string xPath)
+      {
+         IWebElement element = driver.FindElement(By.XPath(xPath)); 
+         element.Submit();
+      }
+      public void SelectByTextXPath(string xPath, string selectedText)
+      {
+         IWebElement element = driver.FindElement(By.XPath(xPath));
+         SelectElement select = new SelectElement(element);
+         select.SelectByText(selectedText);
+      }
+      public void ClickByXPath(string xPath)
+      {
+         IWebElement element = driver.FindElement(By.XPath(xPath)); 
+         element.Click();
+      }
+      public string GetTextByXPath(string xPath)
+      {
+         IWebElement element = driver.FindElement(By.XPath(xPath));
+         return element.Text;
+      }
+      public string GetAttributeByXPath(string xPath, string attribute = "value")
+      {
+         IWebElement element = driver.FindElement(By.XPath(xPath));
+         return element.GetAttribute(attribute);
+      }
+      public bool isDisplayedByXPath(string xPath)
+      {
+         IWebElement element = driver.FindElement(By.XPath(xPath));
+         return element.Displayed;
+      }
+      public bool isEnableByXPath(string xPath)
+      {
+         IWebElement element = driver.FindElement(By.XPath(xPath));
+         return element.Enabled;
+      }
+      public void ClearByXPath(string xPath)
+      {
+         IWebElement element = driver.FindElement(By.XPath(xPath));
+         element.Clear();
+      }
+      [TearDown]
+      public void closeBrowser()
+      {
+         driver.Close();
+         driver.Dispose();
+      }
+
+   }
+}
---
+++ Selenium/SeleniumForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Selenium
+{
+   public enum Operator
+   {
+      Add,
+      Subtract,
+      Multiply,
+      Divide,
+      Concatenate,
+   }
+   public partial class SeleniumForm : Form
+   {
+      public SeleniumForm()
+      {
+         InitializeComponent();
+      }
+      public bool TestCalculatorBuild1(Selenium n,string url, string num1, string num2, Operator @operator, string answer, bool isIntegerOnly = false, bool expectedFailed = false)
+      {
+         try
+         {
+            n.OpenUrl(url);
+            n.SelectByTextXPath("//*[@id=\"selectBuild\"]", "1");
+            n.ClearByXPath("//*[@id=\"number1Field\"]");
+            n.SendKeysByXPath("//*[@id=\"number1Field\"]", num1);
+            n.ClearByXPath("//*[@id=\"number2Field\"]");
+            n.SendKeysByXPath("//*[@id=\"number2Field\"]", num2);
+            n.SelectByTextXPath("//*[@id=\"selectOperationDropdown\"]", @operator.ToString());
+            
+
+            if (@operator != Operator.Concatenate)
+            {
+               bool isEnabled = n.isEnableByXPath("//*[@id=\"integerSelect\"]");
+               if ((isEnabled && !isIntegerOnly) || (!isEnabled && isIntegerOnly))
+               {
+                  n.ClickByXPath("//*[@id=\"integerSelect\"]");
+               }
+            }
+            n.ClickByXPath("//*[@id=\"calculateButton\"]");
+            System.Threading.Thread.Sleep(2000);
+            string actualResult = n.GetAttributeByXPath("//*[@id=\"numberAnswerField\"]");
+            bool result = false;
+            if ((!expectedFailed && actualResult.Trim().Equals(answer)) || (expectedFailed && !actualResult.Trim().Equals(answer)))
+            {
+               result = true;
+            }
+            return result;
+         }catch (Exception ex)
+         {
+            return false;
+         }
+      }
+      private void buttonStart_Click(object sender, EventArgs e)
+      {
+         Selenium n = new Selenium();
+         n.startBrowser();
+         bool rs = TestCalculatorBuild1(n, "https://testsheepnz.github.io/BasicCalculator.html", "1", "1", Operator.Add, "2");
+         richTextBoxResult.Text += "\nTest 1: " + rs.ToString();
+         rs = TestCalculatorBuild1(n, "https://testsheepnz.github.io/BasicCalculator.html", "1.1", "1.2", Operator.Add, "2", true);
+         richTextBoxResult.Text += "\nTest 2: " + rs.ToString();
+         rs = TestCalculatorBuild1(n, "https://testsheepnz.github.io/BasicCalculator.html", "1", "1", Operator.Concatenate, "2");
+         richTextBoxResult.Text += "\nTest 3: " + rs.ToString();
+         n.closeBrowser();
+      }
+   }
+}
